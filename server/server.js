7e444cee31c43e9402fcd8de290683da6ab71729
Anshul_Saxena1 <anshul_saxena1@dell.com>
2018-12-29 12:44:55 +0530

    Greet new user and laert others

--- server/server.js
+++ server/server.js
@@ -11,9 +11,40 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+      from: 'Admin',
+      text: 'Welcome to the chat app',
+      createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+    });
     socket.on('disconnect', () => {
         console.log('Disconnected');
-    })
+    });
+
+    socket.emit('newMessage', {
+      from: 'mike@examole.com',
+      text: 'Hey',
+      createdAt: new Date()
+    });
+
+    socket.on('createMessage', (message) => {
+        console.log('Create message', message);
+        io.emit('newMessage', { from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
+        /* socket.broadcast.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        }); */
+    });
+
 });
 server.listen(port, () => {
   console.log(`Server running on port: ${port}`);
