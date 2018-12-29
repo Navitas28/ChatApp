f0aaee21242adb285f9f2cd05a769ac65040b4d6
Anshul_Saxena1 <anshul_saxena1@dell.com>
2018-12-29 11:02:02 +0530

    Socket changes

--- server/server.js
+++ server/server.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
 const app = express();
+const socketIO = require('socket.io');
 const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '../public');
-
+let server = http.createServer(app);
+let io = socketIO(server);
 app.use(express.static(publicPath));
-app.listen(port, () => {
-    console.log(`Server running on port: ${port}`);
-})
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.on('disconnect', () => {
+        console.log('Disconnected');
+    })
+});
+server.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+});
